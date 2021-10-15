5a78afa5985d6de345cd2c938a92535d18431825
davebenard <benard.zibiri@e4email.net>
2021-10-15 11:00:29 +0100

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeUi from "./views/Home";
 import LayoutUi from "./components/layout/Layout";
 import Store from "./context/GlobalStore";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style/index.scss";
 
 function App() {
